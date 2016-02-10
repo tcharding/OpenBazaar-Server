4c1178fc996e84b7b42fbedaeccf7b4608760415
tcharding <me@tobin.cc>
2016-02-10 20:20:36 +1100

    Modified startup procedure

--- daemon.py
+++ daemon.py
@@ -1,20 +1,49 @@
 __author__ = 'chris'
-import sys, os, time, atexit
+import sys
+import os
+import time
+import atexit
+import socket
+import stun
 from signal import SIGTERM
 
+from obelisk.client import LibbitcoinClient
+from protos.objects import FULL_CONE, RESTRICTED, SYMMETRIC
+from twisted.internet import reactor, task
+from twisted.python import log, logfile
+from txws import WebSocketFactory
 
-class Daemon(object):
-    """
-    A generic daemon class.
+from api.ws import WSFactory
+from api.restapi import RestAPI
+from db.datastore import Database
+from dht.network import Server
+from dht.node import Node
+from dht.storage import PersistentStorage, ForgetfulStorage
+from keys.keychain import KeyChain
+from log import Logger, FileLogObserver
+from market import network
+from market.listeners import MessageListenerImpl, BroadcastListenerImpl, NotificationListenerImpl
+from market.contracts import check_unfunded_for_payment
+from market.profile import Profile
+from net.sslcontext import ChainedOpenSSLContextFactory
+from net.upnp import PortMapper
+from net.utils import looping_retry
+from net.wireprotocol import OpenBazaarProtocol
+from utils.platform_independent import pid_path
+from config import DATA_FOLDER, KSIZE, ALPHA, LIBBITCOIN_SERVER,\
+    LIBBITCOIN_SERVER_TESTNET, SSL_KEY, SSL_CERT, SEEDS, TESTNET,\
+    LOGLEVEL, NETWORK_PORT, ALLOWIP, SSL, RESTAPI_PORT, WEBSOCKET_PORT,\
+    HEARTBEAT_PORT, DAEMON, PIDFILE
 
-    Usage: subclass the Daemon class and override the run() method
-    """
+
+class OpenBazaard(object):
+    """The main OpenBazaar Daemon."""
     # pylint: disable=file-builtin
-    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+    def __init__(self, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
         self.stdin = stdin
         self.stdout = stdout
         self.stderr = stderr
-        self.pidfile = pidfile
+        self.pidfile = PIDFILE
 
     def daemonize(self):
         """
@@ -56,18 +85,11 @@ class Daemon(object):
         os.dup2(so.fileno(), sys.stdout.fileno())
         os.dup2(se.fileno(), sys.stderr.fileno())
 
-        # write pidfile
-        atexit.register(self.delpid)
-        pid = str(os.getpid())
-        file(self.pidfile, 'w+').write("%s\n" % pid)
-
     def delpid(self):
         os.remove(self.pidfile)
 
     def start(self, *args):
-        """
-        Start the daemon
-        """
+        """Start the daemon."""
         # Check for a pidfile to see if the daemon already runs
         try:
             pf = file(self.pidfile, 'r')
@@ -81,8 +103,8 @@ class Daemon(object):
             sys.stderr.write(message % self.pidfile)
             sys.exit(1)
 
-        # Start the daemon
-        self.daemonize()
+        if DAEMON:
+            self.daemonize()
         self.run(*args)
 
     def stop(self):
@@ -123,8 +145,139 @@ class Daemon(object):
         self.stop()
         self.start()
 
-    def run(self, *args):
-        """
-        You should override this method when you subclass Daemon. It will be called after the process has been
-        daemonized by start() or restart().
-        """
+    def run(*args):
+
+        def start_server(keys, first_startup=False):
+            # logging
+            logFile = logfile.LogFile.fromFullPath(DATA_FOLDER + "debug.log", rotateLength=15000000, maxRotatedFiles=1)
+            log.addObserver(FileLogObserver(logFile, level=LOGLEVEL).emit)
+            log.addObserver(FileLogObserver(level=LOGLEVEL).emit)
+            logger = Logger(system="OpenBazaard")
+
+            # NAT traversal
+            p = PortMapper()
+            p.add_port_mapping(NETWORK_PORT, NETWORK_PORT, "UDP")
+            logger.info("Finding NAT Type...")
+
+            response = looping_retry(stun.get_ip_info, "0.0.0.0", NETWORK_PORT)
+
+            logger.info("%s on %s:%s" % (response[0], response[1], response[2]))
+            ip_address = response[1]
+            port = response[2]
+
+            if response[0] == "Full Cone":
+                nat_type = FULL_CONE
+            elif response[0] == "Restric NAT":
+                nat_type = RESTRICTED
+            else:
+                nat_type = SYMMETRIC
+
+            def on_bootstrap_complete(resp):
+                logger.info("bootstrap complete")
+                mserver.get_messages(mlistener)
+                task.LoopingCall(check_unfunded_for_payment, db, libbitcoin_client, nlistener, TESTNET).start(600)
+
+            protocol = OpenBazaarProtocol((ip_address, port), nat_type, testnet=TESTNET,
+                                          relaying=True if nat_type == FULL_CONE else False)
+
+            # kademlia
+            storage = ForgetfulStorage() if TESTNET else PersistentStorage(db.get_database_path())
+            relay_node = None
+            if nat_type != FULL_CONE:
+                for seed in SEEDS:
+                    try:
+                        relay_node = (socket.gethostbyname(seed[0].split(":")[0]),
+                                      28469 if TESTNET else 18469)
+                        break
+                    except socket.gaierror:
+                        pass
+
+            try:
+                kserver = Server.loadState(DATA_FOLDER + 'cache.pickle', ip_address, port, protocol, db,
+                                           nat_type, relay_node, on_bootstrap_complete, storage)
+            except Exception:
+                node = Node(keys.guid, ip_address, port, keys.verify_key.encode(),
+                            relay_node, nat_type, Profile(db).get().vendor)
+                protocol.relay_node = node.relay_node
+                kserver = Server(node, db, keys.signing_key, KSIZE, ALPHA, storage=storage)
+                kserver.protocol.connect_multiplexer(protocol)
+                kserver.bootstrap(kserver.querySeed(SEEDS)).addCallback(on_bootstrap_complete)
+            kserver.saveStateRegularly(DATA_FOLDER + 'cache.pickle', 10)
+            protocol.register_processor(kserver.protocol)
+
+            # market
+            mserver = network.Server(kserver, keys.signing_key, db)
+            mserver.protocol.connect_multiplexer(protocol)
+            protocol.register_processor(mserver.protocol)
+
+            looping_retry(reactor.listenUDP, port, protocol)
+
+            interface = "0.0.0.0" if ALLOWIP not in ("127.0.0.1", "0.0.0.0") else ALLOWIP
+
+            # websockets api
+            authenticated_sessions = []
+            ws_api = WSFactory(mserver, kserver, only_ip=ALLOWIP)
+            ws_factory = AuthenticatedWebSocketFactory(ws_api)
+            ws_factory.authenticated_sessions = authenticated_sessions
+            ws_factory.protocol = AuthenticatedWebSocketProtocol
+            if SSL:
+                reactor.listenSSL(WEBSOCKET_PORT, ws_factory,
+                                  ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
+            else:
+                reactor.listenTCP(WEBSOCKET_PORT, ws_factory, interface=interface)
+
+            # rest api
+            rest_api = RestAPI(mserver, kserver, protocol, username, password,
+                               authenticated_sessions, only_ip=ALLOWIP)
+            if SSL:
+                reactor.listenSSL(REST_PORT, rest_api,
+                                  ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
+            else:
+                reactor.listenTCP(REST_PORT, rest_api, interface=interface)
+
+            # blockchain
+            if TESTNET:
+                libbitcoin_client = LibbitcoinClient(LIBBITCOIN_SERVER_TESTNET, log=Logger(service="LibbitcoinClient"))
+            else:
+                libbitcoin_client = LibbitcoinClient(LIBBITCOIN_SERVER, log=Logger(service="LibbitcoinClient"))
+
+            # listeners
+            nlistener = NotificationListenerImpl(ws_api, db)
+            mserver.protocol.add_listener(nlistener)
+            mlistener = MessageListenerImpl(ws_api, db)
+            mserver.protocol.add_listener(mlistener)
+            blistener = BroadcastListenerImpl(ws_api, db)
+            mserver.protocol.add_listener(blistener)
+
+            protocol.set_servers(ws_api, libbitcoin_client)
+
+            if first_startup:
+                heartbeat_server.push(json.dumps({
+                    "status": "GUID generation complete",
+                    "username": username,
+                    "password": password
+                }))
+
+            heartbeat_server.set_status("online")
+
+            logger.info("Startup took %s seconds" % str(round(time.time() - args[8], 2)))
+
+        # database
+        db = Database(TESTNET)
+
+        # client authentication
+        username, password = get_credentials(db)
+
+        # heartbeat server
+        interface = "0.0.0.0" if ALLOWIP not in ("127.0.0.1", "0.0.0.0") else ALLOWIP
+        heartbeat_server = HeartbeatFactory(only_ip=ALLOWIP)
+        if SSL:
+            reactor.listenSSL(HEARTBEATPORT, WebSocketFactory(heartbeat_server),
+                              ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
+        else:
+            reactor.listenTCP(HEARTBEATPORT, WebSocketFactory(heartbeat_server), interface=interface)
+
+        # key generation
+        KeyChain(db, start_server, heartbeat_server)
+
+        reactor.run()
--- openbazaard.py
+++ openbazaard.py
@@ -1,197 +1,28 @@
-__author__ = 'chris'
+"""Main OpenBazaard process"""
+__author__ = 'chris', 'tobin'
 
 import argparse
-import json
-import platform
-import socket
-import stun
+import requests
 import sys
-import time
-import urllib2
-from api.ws import WSFactory, AuthenticatedWebSocketProtocol, AuthenticatedWebSocketFactory
-from api.restapi import RestAPI
-from config import DATA_FOLDER, KSIZE, ALPHA, LIBBITCOIN_SERVER,\
-    LIBBITCOIN_SERVER_TESTNET, SSL_KEY, SSL_CERT, SEEDS, SSL
-from daemon import Daemon
-from db.datastore import Database
-from dht.network import Server
-from dht.node import Node
-from dht.storage import PersistentStorage, ForgetfulStorage
-from keys.credentials import get_credentials
-from keys.keychain import KeyChain
-from log import Logger, FileLogObserver
-from market import network
-from market.listeners import MessageListenerImpl, BroadcastListenerImpl, NotificationListenerImpl
-from market.contracts import check_unfunded_for_payment
-from market.profile import Profile
-from net.heartbeat import HeartbeatFactory
-from net.sslcontext import ChainedOpenSSLContextFactory
-from net.upnp import PortMapper
-from net.utils import looping_retry
-from net.wireprotocol import OpenBazaarProtocol
-from obelisk.client import LibbitcoinClient
-from protos.objects import FULL_CONE, RESTRICTED, SYMMETRIC
-from twisted.internet import reactor, task
-from twisted.python import log, logfile
-from txws import WebSocketFactory
+
+from daemon import OpenBazaard
+from utils.platform_independent import options_tmp_path
+from config import RESTAPI_PORT
+
+# violates SPOT [Kernighan and Pike 99]
+CONSTANTS = ['data_folder', 'ksize', 'alpha', 'transaction_fee',
+             'libbitcoin_server', 'libbitcoin_server_testnet',
+             'resolver', 'loglevel', 'testnet', 'daemon', 'network_port',
+             'websocket_port', 'restapi_port', 'allowip', 'pidfile']
+AUTHENTICATION = ['ssl', 'ssl_cert', 'ssl_key', 'username', 'password']
 
 
-def run(*args):
-    TESTNET = args[0]
-    LOGLEVEL = args[1]
-    PORT = args[2]
-    ALLOWIP = args[3]
-    RESTPORT = args[4]
-    WSPORT = args[5]
-    HEARTBEATPORT = args[6]
-
-    def start_server(keys, first_startup=False):
-        # logging
-        logFile = logfile.LogFile.fromFullPath(DATA_FOLDER + "debug.log", rotateLength=15000000, maxRotatedFiles=1)
-        log.addObserver(FileLogObserver(logFile, level=LOGLEVEL).emit)
-        log.addObserver(FileLogObserver(level=LOGLEVEL).emit)
-        logger = Logger(system="OpenBazaard")
-
-        # NAT traversal
-        p = PortMapper()
-        p.add_port_mapping(PORT, PORT, "UDP")
-        logger.info("Finding NAT Type...")
-
-        response = looping_retry(stun.get_ip_info, "0.0.0.0", PORT)
-
-        logger.info("%s on %s:%s" % (response[0], response[1], response[2]))
-        ip_address = response[1]
-        port = response[2]
-
-        if response[0] == "Full Cone":
-            nat_type = FULL_CONE
-        elif response[0] == "Restric NAT":
-            nat_type = RESTRICTED
-        else:
-            nat_type = SYMMETRIC
-
-        def on_bootstrap_complete(resp):
-            logger.info("bootstrap complete")
-            mserver.get_messages(mlistener)
-            task.LoopingCall(check_unfunded_for_payment, db, libbitcoin_client, nlistener, TESTNET).start(600)
-
-        protocol = OpenBazaarProtocol(db, (ip_address, port), nat_type, testnet=TESTNET,
-                                      relaying=True if nat_type == FULL_CONE else False)
-
-        # kademlia
-        storage = ForgetfulStorage() if TESTNET else PersistentStorage(db.get_database_path())
-        relay_node = None
-        if nat_type != FULL_CONE:
-            for seed in SEEDS:
-                try:
-                    relay_node = (socket.gethostbyname(seed[0].split(":")[0]),
-                                  28469 if TESTNET else 18469)
-                    break
-                except socket.gaierror:
-                    pass
-
-        try:
-            kserver = Server.loadState(DATA_FOLDER + 'cache.pickle', ip_address, port, protocol, db,
-                                       nat_type, relay_node, on_bootstrap_complete, storage)
-        except Exception:
-            node = Node(keys.guid, ip_address, port, keys.verify_key.encode(),
-                        relay_node, nat_type, Profile(db).get().vendor)
-            protocol.relay_node = node.relay_node
-            kserver = Server(node, db, keys.signing_key, KSIZE, ALPHA, storage=storage)
-            kserver.protocol.connect_multiplexer(protocol)
-            kserver.bootstrap(kserver.querySeed(SEEDS)).addCallback(on_bootstrap_complete)
-        kserver.saveStateRegularly(DATA_FOLDER + 'cache.pickle', 10)
-        protocol.register_processor(kserver.protocol)
-
-        # market
-        mserver = network.Server(kserver, keys.signing_key, db)
-        mserver.protocol.connect_multiplexer(protocol)
-        protocol.register_processor(mserver.protocol)
-
-        looping_retry(reactor.listenUDP, port, protocol)
-
-        interface = "0.0.0.0" if ALLOWIP not in ("127.0.0.1", "0.0.0.0") else ALLOWIP
-
-        # websockets api
-        authenticated_sessions = []
-        ws_api = WSFactory(mserver, kserver, only_ip=ALLOWIP)
-        ws_factory = AuthenticatedWebSocketFactory(ws_api)
-        ws_factory.authenticated_sessions = authenticated_sessions
-        ws_factory.protocol = AuthenticatedWebSocketProtocol
-        if SSL:
-            reactor.listenSSL(WSPORT, ws_factory,
-                              ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
-        else:
-            reactor.listenTCP(WSPORT, ws_factory, interface=interface)
-
-        # rest api
-        rest_api = RestAPI(mserver, kserver, protocol, username, password,
-                           authenticated_sessions, only_ip=ALLOWIP)
-        if SSL:
-            reactor.listenSSL(RESTPORT, rest_api,
-                              ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
-        else:
-            reactor.listenTCP(RESTPORT, rest_api, interface=interface)
-
-        # blockchain
-        if TESTNET:
-            libbitcoin_client = LibbitcoinClient(LIBBITCOIN_SERVER_TESTNET, log=Logger(service="LibbitcoinClient"))
-        else:
-            libbitcoin_client = LibbitcoinClient(LIBBITCOIN_SERVER, log=Logger(service="LibbitcoinClient"))
-
-        # listeners
-        nlistener = NotificationListenerImpl(ws_api, db)
-        mserver.protocol.add_listener(nlistener)
-        mlistener = MessageListenerImpl(ws_api, db)
-        mserver.protocol.add_listener(mlistener)
-        blistener = BroadcastListenerImpl(ws_api, db)
-        mserver.protocol.add_listener(blistener)
-
-        protocol.set_servers(ws_api, libbitcoin_client)
-
-        if first_startup:
-            heartbeat_server.push(json.dumps({
-                "status": "GUID generation complete",
-                "username": username,
-                "password": password
-            }))
-
-        heartbeat_server.set_status("online")
-
-        logger.info("Startup took %s seconds" % str(round(time.time() - args[7], 2)))
-
-    # database
-    db = Database(TESTNET)
-
-    # client authentication
-    username, password = get_credentials(db)
-
-    # heartbeat server
-    interface = "0.0.0.0" if ALLOWIP not in ("127.0.0.1", "0.0.0.0") else ALLOWIP
-    heartbeat_server = HeartbeatFactory(only_ip=ALLOWIP)
-    if SSL:
-        reactor.listenSSL(HEARTBEATPORT, WebSocketFactory(heartbeat_server),
-                          ChainedOpenSSLContextFactory(SSL_KEY, SSL_CERT), interface=interface)
-    else:
-        reactor.listenTCP(HEARTBEATPORT, WebSocketFactory(heartbeat_server), interface=interface)
-
-    # key generation
-    KeyChain(db, start_server, heartbeat_server)
-
-    reactor.run()
-
-if __name__ == "__main__":
-    # pylint: disable=anomalous-backslash-in-string
-    class OpenBazaard(Daemon):
-        def run(self, *args):
-            run(*args)
-
-    class Parser(object):
-        def __init__(self, daemon):
-            self.daemon = daemon
-            parser = argparse.ArgumentParser(
-                description='OpenBazaar-Server v0.1.0',
-                usage='''
+class Parser(object):
+    def __init__(self):
+        self.daemon = OpenBazaard()
+        parser = argparse.ArgumentParser(
+            description='OpenBazaar-Server v0.1.0',
+            usage='''
     python openbazaard.py <command> [<args>]
     python openbazaard.py <command> --help
 
@@ -199,88 +30,126 @@ commands:
     start            start the OpenBazaar server
     stop             shutdown the server and disconnect
     restart          restart the server
-''')
-            parser.add_argument('command', help='Execute the given command')
-            args = parser.parse_args(sys.argv[1:2])
-            if not hasattr(self, args.command):
-                parser.print_help()
-                exit(1)
-            getattr(self, args.command)()
+            ''')
+        parser.add_argument('command', help='Execute the given command')
+        args = parser.parse_args(sys.argv[1:2])
+        if not hasattr(self, args.command):
+            parser.print_help()
+            exit(1)
+        getattr(self, args.command)()
 
-        def start(self):
+    def start(self):
+        parser = argparse.ArgumentParser(
+            description="Start the OpenBazaar server",
+            usage="python openbazaard.py start [<args>]"
+        )
+        parser.add_argument('-d', '--daemon', action='store_true',
+                            help="run the server in the background as a daemon")
+        parser.add_argument('-t', '--testnet', action='store_true',
+                            help="use the test network")
+        parser.add_argument('-s', '--ssl', action='store_true',
+                            help="use ssl on api connections. you must set the path to your "
+                                 "certificate and private key in the config file.")
+        parser.add_argument('-l', '--loglevel',
+                            help="set the logging level [debug, info, warning, error, critical]")
+        parser.add_argument('-p', '--network_port',
+                            help="set the network port")
+        parser.add_argument('-r', '--restapi_port',
+                            help="set the rest api port")
+        parser.add_argument('-w', '--websocket_port',
+                            help="set the websocket api port")
+        parser.add_argument('-b', '--heartbeat_port',
+                            help="set the heartbeat port")
+        parser.add_argument('-a', '--allowip',
+                            help="only allow api connections from this ip")
+        parser.add_argument('--pidfile',
+                            help="name of the pid file")
+        args = parser.parse_args(sys.argv[2:])
 
-            parser = argparse.ArgumentParser(
-                description="Start the OpenBazaar server",
-                usage="python openbazaard.py start [<args>]"
-            )
-            parser.add_argument('-d', '--daemon', action='store_true',
-                                help="run the server in the background as a daemon")
-            parser.add_argument('-t', '--testnet', action='store_true', help="use the test network")
-            parser.add_argument('-l', '--loglevel', default="info",
-                                help="set the logging level [debug, info, warning, error, critical]")
-            parser.add_argument('-p', '--port', help="set the network port")
-            parser.add_argument('-a', '--allowip', default="127.0.0.1",
-                                help="only allow api connections from this ip")
-            parser.add_argument('-r', '--restapiport', help="set the rest api port", default=18469)
-            parser.add_argument('-w', '--websocketport', help="set the websocket api port", default=18466)
-            parser.add_argument('-b', '--heartbeatport', help="set the heartbeat port", default=18470)
-            parser.add_argument('--pidfile', help="name of the pid file", default="openbazaard.pid")
-            args = parser.parse_args(sys.argv[2:])
+        _write_args_to_file(args)
+        _print_splash()
 
-            OKBLUE = '\033[94m'
-            ENDC = '\033[0m'
-            print "________             " + OKBLUE + "         __________" + ENDC
-            print "\_____  \ ______   ____   ____" + OKBLUE + \
-                  "\______   \_____  _____________  _____ _______" + ENDC
-            print " /   |   \\\____ \_/ __ \ /    \\" + OKBLUE +\
-                  "|    |  _/\__  \ \___   /\__  \ \__  \\\_  __ \ " + ENDC
-            print "/    |    \  |_> >  ___/|   |  \    " + OKBLUE \
-                  + "|   \ / __ \_/    /  / __ \_/ __ \|  | \/" + ENDC
-            print "\_______  /   __/ \___  >___|  /" + OKBLUE + "______  /(____  /_____ \(____  (____  /__|" + ENDC
-            print "        \/|__|        \/     \/  " + OKBLUE + "     \/      \/      \/     \/     \/" + ENDC
-            print
-            print "OpenBazaar Server v0.1 starting..."
+        self.daemon.start()
 
-            unix = ("linux", "linux2", "darwin")
+    def stop(self):
+        # pylint: disable=W0612
+        parser = argparse.ArgumentParser(
+            description="Shutdown the server and disconnect",
+            usage='''usage:
+    python openbazaard.py stop''')
+        parser.parse_args(sys.argv[2:])
+        print "OpenBazaar server stopping..."
+        try:
+            request = 'http://localhost:' + RESTAPI_PORT + '/api/v1/shutdown'
+            requests.get(request)
+        except Exception:
+            self.daemon.stop()
 
-            if args.port:
-                port = int(args.port)
-            else:
-                port = 18467 if not args.testnet else 28467
-            if args.daemon and platform.system().lower() in unix:
-                self.daemon.pidfile = "/tmp/" + args.pidfile
-                self.daemon.start(args.testnet, args.loglevel, port, args.allowip,
-                                  int(args.restapiport), int(args.websocketport),
-                                  int(args.heartbeatport), time.time())
-            else:
-                run(args.testnet, args.loglevel, port, args.allowip,
-                    int(args.restapiport), int(args.websocketport),
-                    int(args.heartbeatport), time.time())
+    def restart(self):
+        # pylint: disable=W0612
+        parser = argparse.ArgumentParser(
+            description="Restart the server",
+            usage='''usage:
+    python openbazaard.py restart''')
+        parser.parse_args(sys.argv[2:])
+        print "Restarting OpenBazaar server..."
+        self.daemon.restart()
 
-        def stop(self):
-            # pylint: disable=W0612
-            parser = argparse.ArgumentParser(
-                description="Shutdown the server and disconnect",
-                usage='''usage:
-        python openbazaard.py stop''')
-            parser.add_argument('-r', '--restapiport', help="set the rest api port to shutdown cleanly",
-                                default=18469)
-            args = parser.parse_args(sys.argv[2:])
-            print "OpenBazaar server stopping..."
-            try:
-                request = urllib2.build_opener()
-                request.open('http://localhost:' + args.restapiport + '/api/v1/shutdown')
-            except Exception:
-                self.daemon.stop()
 
-        def restart(self):
-            # pylint: disable=W0612
-            parser = argparse.ArgumentParser(
-                description="Restart the server",
-                usage='''usage:
-        python openbazaard.py restart''')
-            parser.parse_args(sys.argv[2:])
-            print "Restarting OpenBazaar server..."
-            self.daemon.restart()
+def _print_splash():
+    # pylint: disable=anomalous-backslash-in-string
+    OKBLUE = '\033[94m'
+    ENDC = '\033[0m'
+    print "________             " + OKBLUE + "         __________" + ENDC
+    print "\_____  \ ______   ____   ____" + OKBLUE + \
+        "\______   \_____  _____________  _____ _______" + ENDC
+    print " /   |   \\\____ \_/ __ \ /    \\" + OKBLUE +\
+        "|    |  _/\__  \ \___   /\__  \ \__  \\\_  __ \ " + ENDC
+    print "/    |    \  |_> >  ___/|   |  \    " + OKBLUE \
+        + "|   \ / __ \_/    /  / __ \_/ __ \|  | \/" + ENDC
+    print "\_______  /   __/ \___  >___|  /" + OKBLUE + "______  /(____  /_____ \(____  (____  /__|" + ENDC
+    print "        \/|__|        \/     \/  " + OKBLUE + "     \/      \/      \/     \/     \/" + ENDC
+    print
+    print "OpenBazaar Server v0.1 starting..."
 
-    Parser(OpenBazaard('/tmp/openbazaard.pid'))
+
+def _write_args_to_file(args):
+    """
+    Write command line options to file.
+
+    File is temporary and in format accepted by ConfigParser
+    """
+    path = options_tmp_path()
+    f = open(path, 'w+')
+    print >>f, '[CONSTANTS]'
+    for arg in vars(args):
+        if getattr(args, arg):
+               _write_to_file_with_predicate(args, arg, f, _is_constant)
+    print >>f, '[AUTHENTICATION]'
+    for arg in vars(args):
+        if getattr(args, arg):
+               _write_to_file_with_predicate(args, arg, f, _is_authentication)
+               
+    f.close()
+
+def _write_to_file_with_predicate(args, arg, ofile, predicate):
+    """Writes arg to ofile if predicate is true."""
+    if predicate(arg):
+                config_string = arg + ' = ' + str(getattr(args, arg))
+                print >>ofile, config_string
+
+def _is_constant(string):
+    return string in CONSTANTS
+
+def _is_authentication(string):
+    return string in AUTHENTICATION
+
+
+def _config_string_from_arg(args, arg):
+    """Get string in correct config file format."""
+    config_string = arg + ' = ' + str(getattr(args, arg))
+    return config_string
+
+
+if __name__ == "__main__":
+    Parser()
